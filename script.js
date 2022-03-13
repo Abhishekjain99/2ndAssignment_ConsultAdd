7331590d9d937c6144bfa2fdc8a24eed51fb203a
Abhishek Jain <jainabhishek9900@gmail.com>
2022-03-13 21:07:41 +0530

    2nd Assignment Completed

---
+++ script.js
@@ -0,0 +1,11 @@
+function fun()
+{
+   document.getElementById("button1").innerHTML=4;
+   document.getElementById("button2").innerHTML=1;
+   document.getElementById("button3").innerHTML=2;
+   document.getElementById("button4").innerHTML=7;
+   document.getElementById("button6").innerHTML=3;
+   document.getElementById("button7").innerHTML=8;
+   document.getElementById("button8").innerHTML=9;
+   document.getElementById("button9").innerHTML=6;
+}
